8be9d0fd3d0de4885b93fe0b954cfbc3c703b5c3
hil20n <johhilden@gmail.com>
2020-02-03 12:52:31 +0200

    toka

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var uusiHeader = document.createElement('h1');
+uusiHeader.innerHTML = "HARJOITUS OTSIKKO";
+document.body.appendChild(uusiHeader);
+                          
+var uusiBtn = document.createElement('button');
+uusiBtn.innerHTML = "PAINA";
+document.body.appendChild(uusiBtn);
+
+var uusiImg = document.createElement('IMG');
+uusiImg.setAttribute("src", "https://get.musti.media/shops/mfi/resources/ftp/original/56/3b7a1a76bdd9f84327fe335cf81c60.jpg");
+uusiImg.setAttribute("width", "300");
+uusiImg.setAttribute("height", "300");
+document.body.appendChild(uusiImg);
+
+
